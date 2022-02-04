6fb3adad07cb478e265bb2817bce9e8e4abdd976
Theo-Hafsaoui <theo.hafsaoui@laposte.net>
2022-02-04 11:39:54 +0100

    update cm et tp I63

--- L3/I63/tp/tp1/test.py
+++ L3/I63/tp/tp1/test.py
@@ -1,18 +1,35 @@
 import tkinter as tk
 
+
 def create_rectangle(cnv,x1,y1,x2,y2):
     rec=cnv.create_polygon(x1,y1,x1,y2,x2,y2,x2,y1,outline="#fb4934",fill="#fbf1c7")
     cnv.pack()
     return rec
 
+def ABC(x,y,W_x,W_y,Vp_x,Vp_y,HY):
+    """ca pue le y sert a rien"""
+    M_res=[]
+    M_res.append(Vp_x*x**4/W_x)#A
+    M_res.append(-Vp_y*x**4/W_y)#B
+    M_res.append(x**4)#C
+    return M_res
+
 def wc2dc(l_x,vp,wd):
     """
+    assumons que les coordoner de la viewport sont le coins
+    inferieur gauche
     transform plan
+    soit le vecteur  v (u,v,1)
+    et la matrice M
+                  [A B C]
+                  |D E F]
+                  [G H I]
+    le rusltat de v*m et la matrice:
+                  [Au+Bv+C]
+                  |Du Ev F]
+                  [Gu Hv I]
     """
-    u=l_x[1]
-    v=l_x[2]
-    vc=[]
-    vc.append()
+    return vc
 
 
 if __name__ == "__main__":
