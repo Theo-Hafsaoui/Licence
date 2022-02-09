48ec3e7f25324ca9aa731ad02645550bceb5e8d5
Theo-Hafsaoui <theo.hafsaoui@laposte.net>
2022-02-09 09:22:38 +0100

    Todo diferencier origine

--- L3/I63/tp/tp1/test.py
+++ L3/I63/tp/tp1/test.py
@@ -21,8 +21,9 @@ def file2point(file_name):
     l_p=[]
     ls=f.readlines()
     for l in ls:
-        l_p.append(l.split())
-    return l_p
+            l_p.append(l.split())
+    temp=[int(j) for i in l_p for j in i]
+    return temp
 
 def create_point(cnv,x,y):
     """return the object for futher modification,
@@ -46,24 +47,26 @@ def AE_CF(x,y,W_x,W_y,Vp_x,Vp_y):
     matrix that change the space
     for the function wc2dc
     """
-    H=W_y-y
+    #TODO add difff with origini
+    H=Vp_y-y
     M_res=[]
     M_res.append(Vp_x/W_x)#A
-    M_res.append(Vp_y/W_y)#E
+    M_res.append(-Vp_y/W_y)#E
     M_res.append((x*Vp_x)/W_x-x)#C
-    M_res.append((y*Vp_y*-H*Vp_y)/W_y-y)#F
+    M_res.append(-(H*Vp_y+Vp_y*y)/W_y-y)#F
+    print(M_res)
     return M_res
 
-def wc2dc(l_x,vp,wd):
+def wc2nc(l_x,vp,wd):
     """
     return the projection from the
     Euclidian space to the screen one
     """
     w=1#2d space
-    MAE=AE_CF(l_x[0],l_x[1],wd[0],wd[1],vp[0],vp[1])
+    AECF=AE_CF(l_x[0],l_x[1],wd[0],wd[1],vp[0],vp[1])
     Vc=[]
-    Vc.append(MAE[0]*l_x[0]+MAE[1]*l_x[1]+w)#x
-    Vc.append(l_x[0]+MAE[2]*l_x[1]+MAE[3]+w)#x
+    Vc.append(AECF[0]*l_x[0]+AECF[2]*l_x[1]+w*AECF[2])#x
+    Vc.append(l_x[1]+AECF[1]*l_x[1]+AECF[3]+w*AECF[3])#x
     return Vc
 
 #############################################
@@ -74,14 +77,18 @@ if __name__ == "__main__":
     Master = tk.Tk()
     x1,y1=200,200
     x2,y2=400,400
+    l_w=[800,800]
+    l_v=[400,400]
     Master.attributes('-type', 'dialog')
     Master.geometry("800x800")
     cnv=tk.Canvas(Master,height=800,width=800,background="#282828")
     rec=create_rectangle(cnv,x1,y1,x2,y2)
-    Vc=wc2dc(,)
-    create_point(cnv,500,500)
-    cnv.move(rec,200,200)
-    l=file2point("point.point")
-    print(l)
+    list_p=file2point("point.point")
+    for i in range(0,len(list_p),2):
+        Vc=wc2nc([list_p[i],list_p[i+1]],[800,800],[400,400])
+        print(Vc)
+        create_point(cnv,Vc[0],Vc[1])
+    create_point(cnv,400,400)
+    #cnv.move(rec,200,200)
+    #create_point(cnv,100,100)
     Master.mainloop()
-
